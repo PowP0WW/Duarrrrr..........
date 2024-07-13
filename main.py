75508e0f7aeba60a15135c96406b83fcb142a8d4
PowP0WW <firmanwijaya867@gmail.com>
2024-07-13 18:31:32 +0800

    looping and print

---
+++ main.py
@@ -0,0 +1,17 @@
+meme_dict = {
+        "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+        "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+        "GG": "Good Game",
+        "ROFL": "tanggapan terhadap lelucon",
+        "SHEESH": "sedikit ketidaksetujuan",
+        "CREEPY": "menakutkan, tidak menyenangkan",
+        "AGGRO": "untuk menjadi agresif/marah",
+}
+
+#word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")           
+#print(meme_dict["AGGRO"])
+
+for i in range(len(meme_dict)):
+	for i in range(len(meme_dict)):
+    	word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+    	print(meme_dict[word])
